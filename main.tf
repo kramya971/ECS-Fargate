fa15dcd846ea86931d2c864b8f490513da33a400
kramya971 <117531875+kramya971@users.noreply.github.com>
2023-03-27 12:08:48 +0530

    gateway attachment

--- main.tf
+++ main.tf
@@ -35,9 +35,15 @@ resource "aws_internet_gateway" "ecs_igw" {
 }
 
 # Attach the internet gateway to the VPC
-resource "aws_vpc_attachment" "ecs_igw_attachment" {
-  vpc_id = aws_vpc.ecs_vpc.id
+#resource "aws_vpc_attachment" "ecs_igw_attachment" {
+#  vpc_id = aws_vpc.ecs_vpc.id
+#  internet_gateway_id = aws_internet_gateway.ecs_igw.id
+#}
+
+# Attach the internet gateway to the VPC
+resource "aws_internet_gateway_attachment" "ecs" {
   internet_gateway_id = aws_internet_gateway.ecs_igw.id
+  vpc_id              = aws_vpc.ecs_vpc.id
 }
 
 # Create a security group for the ALB
